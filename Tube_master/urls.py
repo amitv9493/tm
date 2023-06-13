f6ffe283cd7256f6f07dbb23215a25d7a73960f6
MehulMachhi <machhimehul12@gmail.com>
2023-06-13 15:54:41 +0530

    add slash besides in wp urls

--- Tube_master/urls.py
+++ Tube_master/urls.py
@@ -193,7 +193,7 @@ urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + 
 #######################################################################
 
     path("api/get/warehouse/", WarehouseListView.as_view(), name="getwarehouse"),
-    path("api/get/warehouse/wp", WarehouseListViewWP.as_view(), name="getwarehousewp"),
+    path("api/get/warehouse/wp/", WarehouseListViewWP.as_view(), name="getwarehousewp"),
     path("api/create/warehouse/", WarehouseCreateView.as_view(), name="createwarehouse"),
     path("api/retupddel/warehouse/<int:pk>/", WarehouseRetUpdDelView.as_view(), name="retupddelwarehouse"),
     path("api/lw/<int:pk>/", WarehouseNewListView.as_view()),
