0ffc014be18e4246d23b7a7bb8e642d16958d2c1
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-06-19 16:37:41 +0530

    update access token time to 30 min

--- Tube_master/settings.py
+++ Tube_master/settings.py
@@ -84,7 +84,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
