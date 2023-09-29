dbfc4b1905e164301660561f9605fe5e3eca25c4
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-29 13:05:48 +0530

    Update wef/ view

--- tm_api/utils.py
+++ tm_api/utils.py
@@ -1,48 +1,26 @@
 from django.db.models import Q, F
 from project.models import Project
+from django.db.models.query import QuerySet
 
-def filter_equipment_by_criteria(model_class, date_obj, slug, pm_status, search, flags):
-    qs = model_class.objects.all()
-
-    if slug:
-        qs = qs.filter(location_for_warehouse__slug=slug)
-
-    if pm_status != "NONE":
-        qs = qs.filter(pm_status=pm_status)
-
-    if search != "None":
-        query = Q()
-        query |= (
-            Q(abbreviation__icontains=search)
-            | Q(alternate_name__icontains=search)
-            | Q(serial_number__icontains=search)
-            | Q(asset_number__icontains=search)
-            | Q(packaging__icontains=search)
-        )
-        qs = qs.filter(query)
+def filter_equipment_by_criteria(model_class, date_obj, p_qs, query, flags) -> QuerySet:
+    qs = model_class.objects.filter(query)
 
     if date_obj:
-        ids = get_ids(date_obj, **flags)
+        ids = get_ids(p_qs, **flags)
         qs = qs.exclude(id__in=ids)
 
     return qs
 
-def get_ids(date_obj, **flags) -> set:
+def get_ids(p_qs, **flags) -> set:
     ids = set()
-    p_qs = Project.objects.annotate(
-        ttd_count=F("ttd__id"),
-        bdd_count=F("bdd__id"),
-        cali_count=F("calibration_stand__id"),
-        swab_count=F("swabmaster_equip__id"),
-    ).filter(equipment_delivery_client__gt=date_obj)
 
     if flags.get("ttds") == 1:
-        ids.update(p_qs.values_list("ttd_count", flat=True))
+        ids.update(p_qs.values_list("ttd", flat=True))
     if flags.get("bdds") == 1:
-        ids.update(p_qs.values_list("bdd_count", flat=True))
+        ids.update(p_qs.values_list("bdd", flat=True))
     if flags.get("calis") == 1:
-        ids.update(p_qs.values_list("cali_count", flat=True))
+        ids.update(p_qs.values_list("calibration_stand", flat=True))
     if flags.get("swabs") == 1:
-        ids.update(p_qs.values_list("swab_count", flat=True))
-
+        ids.update(p_qs.values_list("swabmaster_equip", flat=True))
+    
     return ids
--- tube/views.py
+++ tube/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from tube.serializers import *
 from rest_framework.permissions import DjangoModelPermissions, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from tm_api.utils import filter_equipment_by_criteria, get_ids
 
 class WarehouseView(generics.ListCreateAPIView):
     queryset = Warehouse.objects.all()
@@ -60,55 +60,82 @@ class WarehouseAvailabilityView(generics.ListAPIView):
             return qs.filter(slug=id)
 
         return qs
-    
-from tm_api.utils import filter_equipment_by_criteria
+
+
+
 
 @api_view(["GET"])
 def warehouse_equipment_view(request):
-    slug = request.query_params.get("slug")
-    pm_status = str(request.query_params.get("pm_status")).upper()
-    search = str(request.query_params.get("search"))
-    date_obj = request.query_params.get("date")
+    p_qs = Project.objects.all()
+    slug =  request.query_params.get("slug")
+    pm_status= str(request.query_params.get("pm_status")).upper()
+    search= str(request.query_params.get("search"))
+    date_str =  request.query_params.get("date")
+    
+    query = Q()
+        
+    if slug:
+        query &= Q(location_for_warehouse__slug=slug)
 
-    pagination_class = CustomPagination()
+    if pm_status != "NONE":
+        query &= Q(pm_status=pm_status)
 
+    if search != "None":
+        query |= (
+            Q(abbreviation__icontains=search)
+            | Q(alternate_name__icontains=search)
+            | Q(serial_number__icontains=search)
+            | Q(asset_number__icontains=search)
+            | Q(packaging__icontains=search)
+        )
+        
+    
+    
+    if date_str:
+        p_qs = p_qs.filter(equipment_delivery_client__gt=date_str)
+    
+    
+    params = {
+        "p_qs":p_qs,
+        "query":query,
+        "date_obj":date_str,
+    }
 
     def get_ttd():
-
-        qs = filter_equipment_by_criteria(TTD, date_obj, slug, pm_status, search, {'ttds': 1})
-
+        qs = filter_equipment_by_criteria(flags={"ttds": 1},model_class=TTD, **params)
+        
         serializer = TTDSerializers(qs, many=True)
         return serializer.data
 
     def get_bdd():
+        qs = filter_equipment_by_criteria(flags={"bdds": 1},model_class=BDD, **params)
         
-        qs = filter_equipment_by_criteria(BDD, date_obj, slug, pm_status, search, {'bdds': 1})
         serializer = BDDSerializer(qs, many=True)
         return serializer.data
 
     def get_calibration_stand():
-        
-        qs = filter_equipment_by_criteria(CALIBRATION_STAND, date_obj, slug, pm_status, search, {'cali': 1})
+        qs = filter_equipment_by_criteria(flags={"calis": 1},model_class=CALIBRATION_STAND, **params)
         serializer = CalibrationStandSerializer(qs, many=True)
         return serializer.data
 
     def get_swab_master():
-        
-        qs = filter_equipment_by_criteria(SwabMaster, date_obj, slug, pm_status, search, {'swab': 1})
+        qs = filter_equipment_by_criteria(flags={"swabs": 1},model_class=SwabMaster, **params)
         serializer = SwabMasterSerializer(qs, many=True)
         return serializer.data
-    
+
     ttd_data = get_ttd()
     bdd_data = get_bdd()
     calibration_stand_data = get_calibration_stand()
     swab_master_data = get_swab_master()
-    
+
     ttd_count = len(ttd_data)
     bdd_count = len(bdd_data)
     calibration_stand_count = len(calibration_stand_data)
     swab_master_count = len(swab_master_data)
 
     merged_data = ttd_data + bdd_data + calibration_stand_data + swab_master_data
+
+    pagination_class = CustomPagination()
 
     paginated_data = pagination_class.paginate_queryset(merged_data, request)
 
