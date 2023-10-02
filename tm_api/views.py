5e8004445d995f10307c41e76d0fda186b1629c3
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-10-02 10:30:32 +0530

    Update the equipment view logic

--- tm_api/views.py
+++ tm_api/views.py
@@ -93,7 +93,7 @@ class EquipAndPartGeneralView(ListAPIView):
         project_qs = Project.objects.filter(
             # equipment_prep__gt=start_date,
             equipment_delivery_tubemaster__gt=end_date,
-            equipment_prep__lt=end_date,
+            equipment_prep__lt=start_date,
         )
         if project_slug:
             project_qs = project_qs.exclude(slug=project_slug)
